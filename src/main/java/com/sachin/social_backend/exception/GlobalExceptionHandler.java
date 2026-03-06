a44d74d9a94ec6236ea383b639cfc44b46524c53
Sachin kumar <sachin.sankhla.ug22@nsut.ac.in>
2026-03-07 00:21:44 +0530

    added rate Limit for too many post creation

---
+++ src/main/java/com/sachin/social_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sachin.social_backend.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String,Object>> handleResponseStatusException(ResponseStatusException ex){
+        Map<String,Object> response = new HashMap<>();
+        response.put("status",ex.getStatusCode().value());
+        response.put("error",ex.getReason());
+
+        return new ResponseEntity<>(response,ex.getStatusCode());
+    }
+}
--- src/main/java/com/sachin/social_backend/service/PostService.java
+++ src/main/java/com/sachin/social_backend/service/PostService.java
@@ -4,12 +4,15 @@ import com.sachin.social_backend.entity.Post;
 import com.sachin.social_backend.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -20,6 +23,16 @@ public class PostService {
     private final RedisTemplate<String,Object> redisTemplate;
 
     public Post createPost(String content,String username){
+        String key = "post_rate_limit:" + username;
+        Long count = redisTemplate.opsForValue().increment(key);
+
+        if(count == 1){
+            redisTemplate.expire(key,60, TimeUnit.SECONDS);
+        }
+        if(count > 5){
+            throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,
+                    "Post limit exceeded. try again later.");
+        }
         Post post = Post.builder()
                 .content(content)
                 .username(username)
